c655ad01b171994819574d8d2105d6b60e0d4ef4
unknown <phamquangminh1659@gmail.com>
2022-03-15 17:55:57 +0700

    Handling Envelopes

--- main.cpp
+++ main.cpp
@@ -1,15 +1,5 @@
 #include <iostream>
-
 #include "olcNoiseMaker.h"
-
-std::atomic<double> dFrequencyOutput = 0.0;
-const double dOctaveBaseFrequency = 110.0; // A2
-const double d12thRootOf2 = pow(2.0, 1.0 / 12.0); // beta
-
-// convert frequency into angular velocity
-inline double w(double dHertz) {
-	return dHertz * 2.0 * PI;
-}
 
 enum soundType {
 	SINE_WAVE,
@@ -19,6 +9,82 @@ enum soundType {
 	SAW_WAVE_COMPRESSED,
 	RANDOM_NOISE
 };
+
+struct sEnvelopeADSR {
+	double dAttackTime;
+	double dDecayTime;
+	double dReleaseTime;
+
+	double dSustainAmplitude;
+	double dStartAmplitude;
+
+	double dTriggerOnTime;
+	double dTriggerOffTime;
+
+	bool bKeyHold;
+
+	sEnvelopeADSR() {
+		dAttackTime = 0.01;
+		dDecayTime = 0.01;
+		dStartAmplitude = 1;
+		dSustainAmplitude = 0.8;
+		dReleaseTime = 0.02;
+		dTriggerOnTime = dTriggerOffTime = 0.0;
+		bKeyHold = false;
+	}
+
+	double GetAmplitude(double dTime) {
+		double dAmplitude = 0.0;
+
+
+		// linear system
+		if (bKeyHold) {
+			if (dTime <= dTriggerOnTime + dAttackTime) {
+				dAmplitude = (dTime - dTriggerOnTime) / dAttackTime * dStartAmplitude;
+			}
+			else if (dTime >= dTriggerOnTime + dAttackTime && dTime <= dTriggerOnTime + dAttackTime + dDecayTime) {
+				dAmplitude = dStartAmplitude - (dTime - dTriggerOnTime - dAttackTime) / dDecayTime * (dStartAmplitude - dSustainAmplitude);
+			}
+			else {
+				dAmplitude = dSustainAmplitude;
+			}
+		}		
+		else if (dTime <= dTriggerOffTime + dReleaseTime) {
+			dAmplitude = dSustainAmplitude - (dTime - dTriggerOffTime) / dReleaseTime * dSustainAmplitude;
+		}
+	
+		// refuse to output sound we don't care about
+		if (dAmplitude <= 0.0001) {
+			dAmplitude = 0.0;
+		}
+
+		// defensive programming
+		if (abs(dAmplitude) >= 1.0) {
+			dAmplitude = 0.0;
+		}
+
+		return dAmplitude;
+	}
+
+	void NoteOn(double dTimeOn) {
+		dTriggerOnTime = dTimeOn;
+		bKeyHold = true;
+	}
+	void NoteOff(double dTimeOff) {
+		dTriggerOffTime = dTimeOff;
+		bKeyHold = false;
+	}
+};
+
+std::atomic<double> dFrequencyOutput = 0.0;
+const double dOctaveBaseFrequency = 110.0; // A2
+const double d12thRootOf2 = pow(2.0, 1.0 / 12.0); // beta
+sEnvelopeADSR envelope;
+
+// convert frequency into angular velocity
+inline double w(double dHertz) {
+	return dHertz * 2.0 * PI;
+}
 
 // "type" of sound
 double osc(double dHertz, double dTime, soundType type) {
@@ -46,7 +112,7 @@ double osc(double dHertz, double dTime, soundType type) {
 }
 
 double MakeNoise(double dTime) {
-	double dOutput = osc(dFrequencyOutput, dTime, SQUARE_WAVE) * 0.5;	
+	double dOutput = envelope.GetAmplitude(dTime) * osc(dFrequencyOutput, dTime, SQUARE_WAVE);	
 
 	return dOutput;
 }
@@ -76,21 +142,28 @@ int main()
 	
 	// Add the above keyboard: ZSXCFVGBNJMK,L./, based on virtual key codes:
 	const std::vector<int> keyboard = {0x5A, 0x53, 0x58, 0x43, 0x46, 0x56, 0x47, 0x42, 0x4E, 0x4A, 0x4D, 0x4B, VK_OEM_COMMA, 0x4C, VK_OEM_PERIOD, VK_OEM_2};
-
+	int currentKey = -1;		
 	while (true) {
-		bool bKeyPressed = false;
+		bool bKeyPressed = false;	
 		for (int k = 0; k < keyboard.size(); ++k) {			
 			// detect the pressed key, i.e. the key whose highest bit is set
 			if (GetAsyncKeyState(keyboard[k]) & 0x8000) {
-				// play the note
-				// formula: note_freq = base_note * (beta ^ k)
+				// play the note whose frequency is: note_freq = base_note * (beta ^ k)
 				dFrequencyOutput = dOctaveBaseFrequency * pow(d12thRootOf2, k);
 				bKeyPressed = true;
+				// if the user changes key:
+				if (currentKey != k)
+					envelope.NoteOn(sound.GetTime());
+				currentKey = k;
 			}
-		}
-		// if no keys are pressed, play nothing
+		}	
 		if (!bKeyPressed) {
-			dFrequencyOutput = 0.0;
+			// if the key has just been released
+			if (currentKey != -1) {
+				// play the release phase
+				envelope.NoteOff(sound.GetTime());
+				currentKey = -1;
+			}															
 		}
 	}
 	
